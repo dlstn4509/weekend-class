5763bd0fd755cf06428173ebaa35b924af4a2c34
dlstn4509 <dlstn7609@hanmail.net>
2021-10-10 15:53:30 +0900

    corona

--- 5day/corona/js/common.js
+++ 5day/corona/js/common.js
@@ -1,18 +1,29 @@
 const ctx = document.getElementById('myChart');
 const cities = [];
 const dataArray = [];
+
+function makeCoronaChart(pDate) {
+
+
 $.ajax({
-  url: 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?ServiceKey=Wnus4QpirWGI56CfvzMWDIDHMRL%2FmEF%2FqTl9gwVNbRggLYTGPFIdwBy0L51B%2B27d5QRbLanNmIAxPwNvl7dKPA%3D%3D&startCreateDt=20211009&endCreateDt=20211009&_type=json',
+  url: `http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?ServiceKey=SaqPNX%2Fub%2FFdA3w2TFEoYdecDbMxS0Ex%2B2JhBJhJcD6tJYd13CFIMtwvtiwIwpCMoTiEyULLsp1QEs286MoGsA%3D%3D&startCreateDt=${pDate}&endCreateDt=${pDate}&_type=json`,
   success: function (data) {
-    console.log(data.response.body.items.item)
-    const items =  data.response.body.items.item
+    console.log(data)
+    const items = data.response.body.items.item
+    $('.corona').append(`<ul class="list"></ul>`)
     items.forEach(v => {
       cities.push(v.gubun);
       dataArray.push(v.incDec);
+      /* $('.corona .list').append(`
+      <li>
+      <div class="title">${v.gubun}</div>
+      <div class="title">${v.incDec}</div>
+      </li>
+      `) */
     });
     // chat.js
     const myChart = new Chart(ctx, {
-      type: 'line',
+      type: 'bar',
       data: {
         labels: cities,
         datasets: [{
@@ -28,6 +39,7 @@ $.ajax({
           ]
         }]
       },
+      
       options: {
         scales: {
           y: {
@@ -39,3 +51,68 @@ $.ajax({
     // chat.js 끝
   }
 })
+}
+
+// 달력
+const dayArray = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
+const korDayArray = ['일', '월', '화', '수', '목', '금', '토']
+const monthArray = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+const leapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] // 윤년
+const nonLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+const now = new Date()
+const startDay = new Date(now.getFullYear(), now.getMonth(), 1) // 첫날
+const startYear = startDay.getFullYear()
+const startMonth = now.getMonth()
+
+function makeCal(year, month) {
+
+}
+
+let selectedYear = startDay.getFullYear()
+if(selectedYear%4 === 0) {
+  if(selectedYear%100 === 0) {
+    if(selectedYear%400 === 0) { // 윤년
+      selectedYear = leapYear
+    }
+    else { // 윤년 아님
+      selectedYear = nonLeapYear
+    }
+  }
+  else { // 윤년
+    selectedYear = leapYear
+  }
+}
+else { // 윤년 아님
+  selectedYear = nonLeapYear
+}
+
+dayArray.forEach(v => {
+  $('#datePicker .days .list').append(`<li class=""><span>${v}</span></li>`)
+})
+
+let count = 1
+for (let i=0; i<42; i++) {
+  if(i<startDay.getDay()) { // 공백 넣기
+    $('#datePicker .dates .list').append(`<li class="blank"><span></span></li>`)
+  }
+  else { // 숫자 넣기
+    $('#datePicker .dates .list').append(`<li class=""><span>${count}</span></li>`)
+    count++;
+  }
+  if(count > selectedYear[startMonth]) {
+    break
+  }
+}
+
+function addZero(pNum) {
+  if(pNum < 10) {
+    return '0' + pNum
+  }
+  else {
+    '' + pNum
+  }
+}
+makeCoronaChart(now.getFullYear() + addZero(now.getMonth()) + addZero(now.getDate() - 1))
+
+$('#datePicker')
